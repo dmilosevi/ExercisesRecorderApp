e1da811bdc64bfb45fd467c610258caa0f129467
Dominik Milosevic <dmilosevi@tvz.hr>
2023-08-15 19:03:44 +0200

    - Povezivanje frontend-a i backend-a

--- src/main/java/com/app/exercisesrecorder/controller/ExerciseController.java
+++ src/main/java/com/app/exercisesrecorder/controller/ExerciseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.app.exercisesrecorder.model.Exercise;
 import com.app.exercisesrecorder.service.ExerciseService;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/exercises")
 public class ExerciseController {
 		
