a9cbd9ef3ef74ee761ac2aa41cfd459acd7d0df7
dmilosevic3 <dominik.milosevic@fina.hr>
2023-08-18 14:19:41 +0200

    - kreiranje ExerciseNotFoundAdvice.java klase za hendlanje exceptiona

---
+++ src/main/java/com/app/exercisesrecorder/exception/ExerciseNotFoundAdvice.java
@@ -0,0 +1,25 @@
+package com.app.exercisesrecorder.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ExerciseNotFoundAdvice {
+
+	@ResponseBody
+	@ExceptionHandler(ExerciseNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public Map<String, String> exceptionHandler(ExerciseNotFoundException exception){
+		
+		Map<String, String> errorMap = new HashMap<>();
+		errorMap.put("Error message", exception.getMessage());
+		
+		return errorMap;
+	}
+}
--- src/main/java/com/app/exercisesrecorder/exception/ExerciseNotFoundException.java
+++ src/main/java/com/app/exercisesrecorder/exception/ExerciseNotFoundException.java
@@ -1,6 +1,7 @@
 package com.app.exercisesrecorder.exception;
 
 public class ExerciseNotFoundException extends RuntimeException{
+	
 	public ExerciseNotFoundException(String message) {
 		super(message);
 	}
